0008d189180820c1356294538f2d16197c0e38f5
Андрей Колдырев <koldyrev.andrew@gmail.com>
2018-11-01 18:50:09 +0700

    Add navigation functions to menu

--- VideoGallery/VideoGalleryClient/src/app/menu/menu.component.ts
+++ VideoGallery/VideoGalleryClient/src/app/menu/menu.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
+class MenuItem {
+  name: string;
+  routerNavigator: string;
+  isActive: boolean;
+  constructor (name:string, routerNavigator:string, isActive:boolean) {
+    this.name = name;
+    this.routerNavigator = routerNavigator;
+    this.isActive = isActive;
+  }
+ 
+}
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-
+  isclick : boolean;
   constructor() { }
-
   ngOnInit() {
+  };
+  changeActive(item: MenuItem)
+  {
+    item.isActive = !item.isActive;
   }
 
+  public MenuItemsArr  = [
+    new MenuItem ("Home", "/greetings", true),
+    new MenuItem ("FilmSearch", "/searchFilms", false),
+    new MenuItem ("Contacts", "/contacts", false),
+  ];
 }
+
+
+
+
