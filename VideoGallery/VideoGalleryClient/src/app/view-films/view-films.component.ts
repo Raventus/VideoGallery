ef8faafe9c81003ca8743450409543219d5e2cc7
Андрей Колдырев <koldyrev.andrew@gmail.com>
2018-11-13 17:49:47 +0700

    Start work for ViewFilm component

--- VideoGallery/VideoGalleryClient/src/app/view-films/view-films.component.ts
+++ VideoGallery/VideoGalleryClient/src/app/view-films/view-films.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpRequestServer} from '../model/http-request-model';
+import {Observable} from "rxjs";
+import {FilmModelAbstract} from '../model/film-model/abstract/film-model-abstract';
+import {PlatformModelAbstract, ParameterItem} from '../model/platform-model/abstract/platform-model-abstract';
 
 @Component({
   selector: 'app-view-films',
@@ -7,9 +11,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewFilmsComponent implements OnInit {
 
-  constructor() { }
+  
+
+  
+  FilmCollection: FilmModelAbstract[];
+  
+  constructor(private httpRequest: HttpRequestServer) { 
+    this.httpRequest.Get().subscribe(collection => {
+      this.FilmCollection = collection});
+  }
 
   ngOnInit() {
+
   }
 
 }
