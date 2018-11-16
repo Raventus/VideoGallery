9143540749a5a0afd1e874d4aefa3e92f2c4946a
Andrew Koldyrev <koldyrev.andrew@gmail.com>
2018-11-16 22:22:57 +0700

    Add form validation

--- VideoGallery/VideoGalleryClient/src/app/search-film/search-film.component.ts
+++ VideoGallery/VideoGalleryClient/src/app/search-film/search-film.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ApplicationRef } from '@angular/core';
 import { Router } from "@angular/router";
 import {PlatformModelAbstract, ParameterItem} from '../model/platform-model/abstract/platform-model-abstract';
 import {SearhModelAbstractService} from '../model/search-model/abstract/searh-model-abstract.service';
 import {HttpRequestServer} from '../model/http-request-model';
 import { NgForm } from "@angular/forms";
+import { isJsObject } from '@angular/core/src/change_detection/change_detection_util';
 
 @Component({
   selector: 'app-search-film',
@@ -12,7 +13,11 @@ import { NgForm } from "@angular/forms";
 })
 export class SearchFilmComponent implements OnInit {
 
-  public errorMessage: string;
+   errorMessage: string;
+
+   formSubmitted: boolean;
+
+
 
   constructor(private router: Router, private _platform :PlatformModelAbstract, private httpRequest: HttpRequestServer) { }
 
@@ -25,15 +30,52 @@ export class SearchFilmComponent implements OnInit {
   }
   DoSearch(form: NgForm)
   {
+    this.formSubmitted = true;
     if (form.valid) {
       console.log(this._platform.GetSearchModel().filmName);
       this.router.navigateByUrl("viewFilms");
+      this.formSubmitted = false;
     }
     else {
+     // form.reset;
+      
       this.errorMessage = "Form Data Invalid";
     }
   }
-  
+
+  getFormValidationMessages (form: NgForm): string[] {
+    let messages : string[] = [];
+    Object.keys(form.controls).forEach (item => {
+      this.getValidationMessage (form.controls[item], item).forEach(element => messages.push(element));
+        
+      });
+      return messages;
+    }
+
+  getValidationMessage (state: any, thingName?: string) {
+    let thing: string = state.path || thingName;
+    let messages: string[] = [];
+    if (state.errors) {
+        for (let errorName in state.errors) {
+          switch (errorName) {
+            case "required":
+              messages.push (`You must enter a ${thing}`);
+              break;
+            case "minlength":
+              messages.push (`A ${thing} must be at least ${state.errors['minlength'].requiredLength} characters`);
+              break;
+            case 'pattern':
+              messages.push(`The ${thing} contains illegal characters`);
+              break;
+          }
+        }
+    }
+    console.log (messages);
+    return messages;
+  }
+
+
+
   ngOnInit() {
     console.log("Init search film component");
   }
